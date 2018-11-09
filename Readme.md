221001a47231ccd986e400f9e48bb6ca1f3c3a3e
maximiloi <maximiloi@gmail.com>
2018-11-09 13:23:44 +0300

    first commit

---
+++ Readme.md
@@ -0,0 +1,11 @@
+# Начальный шаблон для работы
+
+gulp
+less
+
+npm init
+npm i
+
+npx gulp run
+npm start
+npm test
---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+"use strict";
+
+var gulp = require("gulp");
+var less = require("gulp-less");
+var sourcemaps = require("gulp-sourcemaps");
+var plumber = require("gulp-plumber");
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+var server = require("browser-sync").create();
+
+gulp.task("style", function() {
+  gulp.src("source/less/style.less")
+    .pipe(sourcemaps.init())
+    .pipe(plumber())
+    .pipe(less())
+    .pipe(postcss([
+      autoprefixer()
+    ]))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest("source/css"))
+    .pipe(server.stream());
+});
+
+gulp.task("serve", ["style"], function() {
+  server.init({
+    server: "source/",
+    notify: false,
+    open: true,
+    cors: true,
+    ui: false
+  });
+
+  gulp.watch("source/less/**/*.less", ["style"]);
+  gulp.watch("source/*.html").on("change", server.reload);
+});
